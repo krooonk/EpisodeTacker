95f9625883b8e1f215932888d4de66bb328f9e8b
Nane <nanepoghossian@gmail.com>
2026-03-16 20:49:05 +0400

    Add SQLAlchemy config to __init_.py

--- episodeTrackerApp/__init__.py
+++ episodeTrackerApp/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from models import db
+import os
 
-app=Flask(__name__)
+ROOT=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+app=Flask(__name__, instance_path=os.path.join(ROOT, "instance"))
+
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///episodetracker.db"
+db.init_app(app)
 
 from episodeTrackerApp import routes
