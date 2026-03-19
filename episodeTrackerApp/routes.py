6bf24af07916b60eb664bce6927e576ecec7d013
Nane <nanepoghossian@gmail.com>
2026-03-19 18:29:24 +0400

    Add genre filter to series page

--- episodeTrackerApp/routes.py
+++ episodeTrackerApp/routes.py
@@ -1,6 +1,6 @@
 from episodeTrackerApp import app
 from flask import render_template,request
-from models import Series
+from models import Series,Genre
 
 @app.route("/")
 @app.route("/home")
@@ -9,6 +9,11 @@ def home_page():
 
 @app.route("/series")
 def series_page():
+    genres=Genre.query.all()
+    selected_genres=request.args.getlist("genre",type=int)
+    query=Series.query
+    if selected_genres:
+        query=query.filter(Series.genres.any(Genre.id.in_(selected_genres)))
     page=request.args.get("page",1,type=int)
-    series=Series.query.paginate(page=page,per_page=24)
-    return render_template("series.html",series=series)
+    series=query.paginate(page=page,per_page=24)
+    return render_template("series.html",series=series,genres=genres,selected_genres=selected_genres)
