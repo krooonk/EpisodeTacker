05dc7cfeee15e0ac8e386e019dd34365ce2ba0db
Nane <nanepoghossian@gmail.com>
2026-03-21 20:04:26 +0400

    Add registration form

--- episodeTrackerApp/forms.py
+++ episodeTrackerApp/forms.py
@@ -1,1 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField
+from wtforms.validators import Length,DataRequired,Email,EqualTo
+
+class RegistrationForm(FlaskForm):
+    username=StringField("Username",validators=[Length(3,25),DataRequired()])
+    email=StringField("Email",validators=[Email(),DataRequired()])
+    password=PasswordField("Password",validators=[DataRequired()])
+    confirm_password=PasswordField("Repeat the Password",validators=[DataRequired(),EqualTo("password",message="Passwords must match.")])
 
